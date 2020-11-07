53bbb10420c046c6470341f266fd25b73f168729
Eugene Kolesnyk <eugeniusz.kolesnyk@gmail.com>
2020-11-07 11:09:24 +0100

    Added preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { SignIn, Home } from 'pages'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
+  useEffect(() => {
+    const ele = document.getElementById('preloader')
+    if (ele) {
+      // fade out
+      ele?.classList.add('available')
+      setTimeout(() => {
+        // remove from DOM
+        if (ele) ele.outerHTML = ''
+      }, 2000)
+    }
+  }, [])
   return (
     <div className="App">
       <Switch>
